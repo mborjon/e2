2a102b279ad7efbbbb0b829bd7ec57cc5d87e058
Melinda Heinlein <melinda@alumni.ucla.edu>
2020-09-17 13:54:47 -0700

    initialized p1

--- p1/index.php
+++ p1/index.php
@@ -1,11 +1,9 @@
-!<!doctype html>
+<!doctype html>
 <html lang='en'>
 <head><title>Practice Application</title>
     <meta charset='utf-8'>
 </head>
 <body>
     <h1>Practice Application</h1>
-    <img src='/images/hes-logo.png' alt='HES Logo'>
-
 </body>
 </html>
